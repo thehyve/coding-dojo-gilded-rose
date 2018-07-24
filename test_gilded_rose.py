85159f5eb83485c7ece0e38ba3e70bfec3231cdf
Ruslan Forostianov <ruslan@thehyve.nl>
2018-07-24 19:26:47 +0200

    add the functionality test

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -39,5 +39,44 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertLess(items[0].sell_in, 0)
 
+class GildedRoseApprovalTest(unittest.TestCase):
+    "Test functionality on a mixed set of items"
+
+    def test_various_items(self):
+        "Test that sensible results come out in a mixed list"
+        # given
+        items = [
+            Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
+            Item(name="Aged Brie", sell_in=2, quality=0),
+            Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
+            Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+            Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
+            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
+            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+            Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
+        ]
+        gilded_rose = GildedRose(items)
+        # when
+        gilded_rose.update_quality()
+        # then
+        items_expected = [
+            Item(name="+5 Dexterity Vest", sell_in=9, quality=19),
+            Item(name="Aged Brie", sell_in=1, quality=1),
+            Item(name="Elixir of the Mongoose", sell_in=4, quality=6),
+            Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+            Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=14, quality=21),
+            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=9, quality=50),
+            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=4, quality=50),
+            Item(name="Conjured Mana Cake", sell_in=2, quality=5),  # <-- :O
+        ]
+
+        self.assertListEqual(
+            [repr(item) for item in gilded_rose.items],
+            [repr(item) for item in items_expected])
+
+
+
 if __name__ == '__main__':
     unittest.main()
