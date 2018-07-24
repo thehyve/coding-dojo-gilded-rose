16be8b6e70a8307e558d2ac5a16c939e50d2f0c6
Ruslan Forostianov <ruslan@thehyve.nl>
2018-07-24 18:42:45 +0200

    initial commit

---
+++ test_gilded_rose.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from gilded_rose import Item, GildedRose
+
+class GildedRoseTest(unittest.TestCase):
+    def test_foo(self):
+        "Check if item foo is still in items after update quality"
+        items = [Item("foo", 0, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual("foo", items[0].name)
+
+if __name__ == '__main__':
+    unittest.main()
