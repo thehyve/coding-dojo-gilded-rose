c3f031d8ef18f80e47d333f2c3b66c9c9865d24b
Ruslan Forostianov <ruslan@thehyve.nl>
2018-07-31 18:36:33 +0200

    add day three to approval test

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -76,6 +76,23 @@ class GildedRoseApprovalTest(unittest.TestCase):
             [repr(item) for item in gilded_rose.items],
             [repr(item) for item in items_expected])
 
+        gilded_rose.update_quality()
+
+        items_expected = [
+            Item(name="+5 Dexterity Vest", sell_in=8, quality=18),
+            Item(name="Aged Brie", sell_in=0, quality=2),
+            Item(name="Elixir of the Mongoose", sell_in=3, quality=5),
+            Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+            Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=13, quality=22),
+            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=8, quality=50),
+            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=3, quality=50),
+            Item(name="Conjured Mana Cake", sell_in=1, quality=4),  # <-- :O
+        ]
+
+        self.assertListEqual(
+            [repr(item) for item in gilded_rose.items],
+            [repr(item) for item in items_expected])
 
 
 if __name__ == '__main__':
