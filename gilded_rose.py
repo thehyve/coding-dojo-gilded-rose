12ba1b2d86d3d574c9787276b04a83b2078b71ad
Ruslan Forostianov <ruslan@thehyve.nl>
2018-07-31 19:19:40 +0200

    add decrease quality method

--- gilded_rose.py
+++ gilded_rose.py
@@ -5,16 +5,20 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
+    def _decrease_quality(self, item):
+        if item.quality > 0:
+            item.quality -= 1
+
     @staticmethod
     def get_degrade_factor(item):
         return 1
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+            degrades_with_normal_rate = item.name not in {"Aged Brie", "Backstage passes to a TAFKAL80ETC concert",
+                                                          "Sulfuras, Hand of Ragnaros"}
+            if degrades_with_normal_rate:
+                self._decrease_quality(item)
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
