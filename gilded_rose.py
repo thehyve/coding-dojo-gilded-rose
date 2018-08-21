c70fa711b6fae21a1c40dcd71abe517e29f1a40d
Ruslan Forostianov <ruslan@thehyve.nl>
2018-08-21 19:05:20 +0200

    remove code that should be in _update_quality

--- gilded_rose.py
+++ gilded_rose.py
@@ -40,17 +40,6 @@ class GildedRose(object):
             self._update_quality(item)
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                #else:
-                #    self._increase_quality(item)
-
 
 class Item:
     def __init__(self, name, sell_in, quality):
